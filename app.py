15b30b873814850fb15767938433ad8865c32a58
Srija Kothakonda <Srija-2303A52189@sru.edu.in>
2026-06-21 21:23:46 +0530

    Add sample dataset

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.markdown("---")
 
 @st.cache_data
 def load_data():
-    data = pd.read_csv("creditcard.csv")
+    data = pd.read_csv("creditcard_sample.csv")
     return data
 
 @st.cache_resource
